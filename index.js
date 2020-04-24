ae1ff0d043c557300b8f478ad6804c61a32cc228
TheJoeCoder <33997308+TheJoeCoder@users.noreply.github.com>
2020-04-24 20:19:41 +0100

    Added proxy starter function

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
-module.exports.Proxy = require('./src/Proxy.js')
-module.exports.createProxy = require('./src/createProxy.js')
+const MCProxy = require('./src/index.js');
+
+let localServerOptions = {
+  'port': '25578',
+  'version': '1.12.2',
+  'online-mode': false,
+  'motd': 'nodejs minecraft proxy'
+}
+
+let serverList = {
+  hub: {
+    host: 'localhost',
+    port: 25565,
+    isDefault: true,
+    isFallback: true
+  },
+  minigames: {
+    host: 'localhost',
+    port: 25566
+  }
+}
+
+// if you leave proxyOptions empty yo may as well not pass it in the arguments, I wrote it anyway to point out that it exist
+let proxyOptions = {}
+
+/*
+  Use the "/server <serverName>" command in chat to move between servers.
+  <serverName> is the name that you chose for the server inside the serverList
+  This command is implemented by /src/Plugins/ChatCommands.js and it can be disabled by setting enablePlugin: false inside proxyOptions
+*/
+let proxy = MCProxy.createProxy(localServerOptions, serverList, proxyOptions);
+
+proxy.on('error', console.error);
+
+proxy.on('listening', () => {
+  console.info('Listening!');
+});
